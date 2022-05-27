5ba80550524249893a27433ebf516bf3fcd65a8e
PolinaDruzhinina <pvdruzhinina@gmail.com>
2022-05-27 22:11:18 +0300

    added nvidia unet

---
+++ vanila_nnunet_torchio/utils.py
@@ -0,0 +1,281 @@
+from pytorch_lightning.callbacks import Callback
+import torch
+from torch import nn
+from torch.nn.modules.loss import CrossEntropyLoss
+from surface_distance import metrics
+import numpy as np 
+
+
+
+class MyPrintingCallback(Callback):
+    def on_init_start(self, trainer):
+        print("Starting to init trainer!")
+
+    def on_init_end(self, trainer):
+        print("Trainer is init now")
+
+    def on_train_end(self, trainer, pl_module):
+        print("The training is over.")
+
+def sum_tensor(inp, axes, keepdim=False):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/tensor_utilities.py
+    axes = np.unique(axes).astype(int)
+    if keepdim:
+        for ax in axes:
+            inp = inp.sum(int(ax), keepdim=True)
+    else:
+        for ax in sorted(axes, reverse=True):
+            inp = inp.sum(int(ax))
+    return inp
+
+def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
+    """
+    net_output must be (b, c, x, y(, z)))
+    gt must be a label map (shape (b, 1, x, y(, z)) OR shape (b, x, y(, z))) or one hot encoding (b, c, x, y(, z))
+    if mask is provided it must have shape (b, 1, x, y(, z)))
+    :param net_output:
+    :param gt:
+    :param axes:
+    :param mask: mask must be 1 for valid pixels and 0 for invalid pixels
+    :param square: if True then fp, tp and fn will be squared before summation
+    :return:
+    """
+    if axes is None:
+        axes = tuple(range(2, len(net_output.size())))
+
+    shp_x = net_output.shape
+    shp_y = gt.shape
+
+    with torch.no_grad():
+        if len(shp_x) != len(shp_y):
+            gt = gt.view((shp_y[0], 1, *shp_y[1:]))
+
+        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+            # if this is the case then gt is probably already a one hot encoding
+            y_onehot = gt
+        else:
+            gt = gt.long()
+            y_onehot = torch.zeros(shp_x)
+            if net_output.device.type == "cuda":
+                y_onehot = y_onehot.cuda(net_output.device.index)
+            y_onehot.scatter_(1, gt, 1)
+
+    tp = net_output * y_onehot
+    fp = net_output * (1 - y_onehot)
+    fn = (1 - net_output) * y_onehot
+
+    if mask is not None:
+        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
+        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
+        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
+
+    if square:
+        tp = tp ** 2
+        fp = fp ** 2
+        fn = fn ** 2
+
+    tp = sum_tensor(tp, axes, keepdim=False)
+    fp = sum_tensor(fp, axes, keepdim=False)
+    fn = sum_tensor(fn, axes, keepdim=False)
+
+    return tp, fp, fn
+
+def flatten(tensor):
+    """Flattens a given tensor such that the channel axis is first.
+    The shapes are transformed as follows:
+       (N, C, D, H, W) -> (C, N * D * H * W)
+    """
+    C = tensor.size(1)
+    # new axis order
+    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
+    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
+    transposed = tensor.permute(axis_order).contiguous()
+    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
+    return transposed.view(C, -1)
+
+# def dsc(y_true, y_pred):
+#      smooth = 1.
+#      y_true_f = K.flatten(y_true)
+#      y_pred_f = K.flatten(y_pred)
+#      intersection = K.sum(y_true_f * y_pred_f)
+#      score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+#      return score
+
+# def dice_loss(y_true, y_pred):
+#     return (1 - dsc(y_true, y_pred))
+
+class GDiceLossV2(nn.Module):
+    def __init__(self, apply_nonlin=None, smooth=1e-5):
+        """
+        Generalized Dice;
+        Copy from: https://github.com/wolny/pytorch-3dunet/blob/6e5a24b6438f8c631289c10638a17dea14d42051/unet3d/losses.py#L75
+        paper: https://arxiv.org/pdf/1707.03237.pdf
+        tf code: https://github.com/NifTK/NiftyNet/blob/dev/niftynet/layer/loss_segmentation.py#L279
+        """
+        super(GDiceLossV2, self).__init__()
+
+        self.apply_nonlin = apply_nonlin
+        self.smooth = smooth
+
+    def forward(self, net_output, gt):
+        shp_x = net_output.shape # (batch size,class_num,x,y,z)
+        shp_y = gt.shape # (batch size,1,x,y,z)
+        # one hot code for gt
+        with torch.no_grad():
+            if len(shp_x) != len(shp_y):
+                gt = gt.view((shp_y[0], 1, *shp_y[1:]))
+
+            if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+                # if this is the case then gt is probably already a one hot encoding
+                y_onehot = gt
+            else:
+                gt = gt.long()
+                y_onehot = torch.zeros(shp_x)
+                if net_output.device.type == "cuda":
+                    y_onehot = y_onehot.cuda(net_output.device.index)
+                y_onehot.scatter_(1, gt, 1)
+
+
+        if self.apply_nonlin is not None:
+            net_output = self.apply_nonlin(net_output)
+
+        input = flatten(net_output)
+        target = flatten(y_onehot)
+        target = target.float()
+        target_sum = target.sum(-1)
+        class_weights = Variable(1. / (target_sum * target_sum).clamp(min=self.smooth), requires_grad=False)
+
+        intersect = (input * target).sum(-1) * class_weights
+        intersect = intersect.sum()
+
+        denominator = ((input + target).sum(-1) * class_weights).sum()
+
+        return  - 2. * intersect / denominator.clamp(min=self.smooth)
+    
+class SoftDiceLoss(nn.Module):
+    def __init__(self, apply_nonlin=None, batch_dice=False, do_bg=True, smooth=1.,
+                 square=False):
+        """
+        paper: https://arxiv.org/pdf/1606.04797.pdf
+        """
+        super(SoftDiceLoss, self).__init__()
+
+        self.square = square
+        self.do_bg = do_bg
+        self.batch_dice = batch_dice
+        self.apply_nonlin = apply_nonlin
+        self.smooth = smooth
+
+    def forward(self, x, y, loss_mask=None):
+        shp_x = x.shape
+
+        if self.batch_dice:
+            axes = [0] + list(range(2, len(shp_x)))
+        else:
+            axes = list(range(2, len(shp_x)))
+
+        if self.apply_nonlin is not None:
+            x = self.apply_nonlin(x)
+
+        tp, fp, fn = get_tp_fp_fn(x, y, axes, loss_mask, self.square)
+
+        dc = (2 * tp + self.smooth) / (2 * tp + fp + fn + self.smooth)
+
+        if not self.do_bg:
+            if self.batch_dice:
+                dc = dc[1:]
+            else:
+                dc = dc[:, 1:]
+        dc = dc.mean()
+
+        return -dc
+       
+def softmax_helper(x):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/nd_softmax.py
+    rpt = [1 for _ in range(len(x.size()))]
+    rpt[1] = x.size(1)
+    x_max = x.max(1, keepdim=True)[0].repeat(*rpt)
+    e_x = torch.exp(x - x_max)
+    return e_x / e_x.sum(1, keepdim=True).repeat(*rpt)
+
+
+class DC_and_CE_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, ce_kwargs, aggregate="sum"):
+        super(DC_and_CE_loss, self).__init__()
+        self.aggregate = aggregate
+        self.ce = CrossEntropyLoss()
+        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+    def forward(self, net_output, target):
+        dc_loss = self.dc(net_output, target)
+        ce_loss = self.ce(net_output, target.squeeze(1).long())
+        print(dc_loss), print(ce_loss)
+        result = 0.5*ce_loss + 0.5*dc_loss
+        
+        return result
+
+class Criterion:
+    def __init__(self,delta = 0.5):
+        """
+        Args:          
+        """
+        self.delta=delta
+        self.ce_loss = CrossEntropyLoss()
+        self.dice_loss= DiceLoss(4)
+        
+    def __call__(self, logits, targets):
+        # logits_arg = torch.argmax(logits, 1).unsqueeze(1).type_as(targets)
+        # loss_dice = 1 - metrics.compute_dice_coefficient(targets, logits_arg)
+        loss_dice,_ = self.dice_loss(logits, targets.squeeze(1).long(), softmax=True)
+        return self.delta * self.ce_loss(logits, targets.squeeze(1).long()) + self.delta* loss_dice
+    
+class BCE:
+    def __init__(self,delta = 0.5):
+        """
+        Args:          
+        """
+        self.ce_loss = CrossEntropyLoss()
+        
+    def __call__(self, logits, targets):
+        # logits_arg = torch.argmax(logits, 1).unsqueeze(1).type_as(targets)
+        # loss_dice = 1 - metrics.compute_dice_coefficient(targets, logits_arg)
+        # loss_dice,_ = self.dice_loss(logits, targets.squeeze(1).long(), softmax=True)
+        return self.ce_loss(logits, targets.squeeze(1).long())
+    
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(), target.size())
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+#             print(dice), print('dice')
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes, class_wise_dice
